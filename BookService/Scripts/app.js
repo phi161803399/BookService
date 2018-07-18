696e5e2e31702340e799f8ebb2f0e719c2540dee
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-07-18 13:50:49 +0200

    AddAddBook

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -2,6 +2,7 @@
     var self = this;
     self.books = ko.observableArray();
     self.error = ko.observable();
+    self.detail = ko.observable();
 
     var booksUri = '/api/books/';
 
@@ -24,8 +25,48 @@
         });
     }
 
+    self.getBookDetail = function(item) {
+        ajaxHelper(booksUri + item.Id, 'GET').done(function(data) {
+            self.detail(data);
+        });
+    }
+
     // Fetch the initial data.
     getAllBooks();
+
+
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
+
+    var authorsUri = '/api/authors/';
+
+    function getAuthors() {
+        ajaxHelper(authorsUri, 'GET').done(function (data) {
+            self.authors(data);
+        });
+    }
+
+    self.addBook = function (formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year()
+        };
+
+        ajaxHelper(booksUri, 'POST', book).done(function (item) {
+            self.books.push(item);
+        });
+    }
+
+    getAuthors();
 };
 
 ko.applyBindings(new ViewModel());
