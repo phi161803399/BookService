755d729ce9e7503e07eaece88cfda3d0aa760059
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-07-18 11:17:10 +0200

    AddBooksController

--- BookService/Models/BookServiceContext.cs
+++ BookService/Models/BookServiceContext.cs
@@ -20,5 +20,7 @@ namespace BookService.Models
         }
 
         public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<BookService.Models.Book> Books { get; set; }
     }
 }
